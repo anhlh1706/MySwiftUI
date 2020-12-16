45e897e4e4b6b2db514648c2bb62b06a3c0ea173
Hoang Anh <anhlh1706@gmail.com>
2020-12-16 09:18:41 +0700

    Initial Commit

---
+++ Shared/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  Shared
+//
+//  Created by Lê Hoàng Anh on 16/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
