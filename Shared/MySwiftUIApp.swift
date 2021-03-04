3b74acbe58d8c2de9ed0d532274b6438c1e4b688
Hoang Anh <anhlh1706@gmail.com>
2021-03-04 14:45:17 +0700

    implement AppDelegate

--- Shared/MySwiftUIApp.swift
+++ Shared/MySwiftUIApp.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 @main
 struct MySwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(Global())
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Launched")
+        return true
     }
 }
