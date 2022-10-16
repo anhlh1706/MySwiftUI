b0fc9a8ad97ee7427bce0484a5fe237346af9e8c
Bym <anhlh1706@gmail.com>
2022-10-16 23:30:45 +0700

    adding new shadow factor in ios16

--- Shared/DesignCode/Shadow.swift
+++ Shared/DesignCode/Shadow.swift
@@ -9,23 +9,38 @@ import SwiftUI
 
 struct ShadowView: View {
     var body: some View {
-        ZStack {
-            Color.white
+        VStack {
+            ZStack {
+                Color.white
+                
+                VStack {
+                    Text("App of the day")
+                        .font(.title).bold()
+                        .foregroundColor(.white)
+                        .shadow(radius: 20)
+                }
+                .frame(width: 300, height: 200)
+                .background(Color.pink)
+                .cornerRadius(20)
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                .shadow(color: Color.pink.opacity(0.3), radius: 20, x: 0, y: 10)
+                .mask(Color.black.opacity(0.3))
+            }
             
-            VStack {
+            if #available(iOS 16, *) {
                 Text("App of the day")
                     .font(.title).bold()
                     .foregroundColor(.white)
                     .shadow(radius: 20)
+                    .overlay(content: {
+                        Circle()
+                            .fill(.pink.shadow(.drop(color: .black.opacity(0.5), radius: 10)))
+                            .frame(width: 300, height: 200)
+                    })
             }
-            .frame(width: 300, height: 400)
-            .background(Color.pink)
-            .cornerRadius(20)
-            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-            .shadow(color: Color.pink.opacity(0.3), radius: 20, x: 0, y: 10)
-            .mask(Color.black.opacity(0.3))
+            
+            Spacer()
         }
-        
     }
 }
 
